60879dbd36338eec59205cb0e31c84ed1ed239c0
kanamaka <kubira12@gmail.com>
2022-01-28 19:00:11 +0900

    アコーディオンとりあえず完成

--- js/join.js
+++ js/join.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
 	$('.slider').slick({
-			autoplay: true
 	});
 });
 
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 2,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+	autoplay: true,
+  autoplaySpeed: 2000,
 });
+
+$(".js-menu").on("click", function () {
+	$(this).next().slideToggle();
+ });
