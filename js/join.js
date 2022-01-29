3bca772347d85c1790e5ae8ec911563dcdc9b7be
kanamaka <kubira12@gmail.com>
2022-01-30 03:55:36 +0900

    機能は完成、レイアウト修正実施

--- js/join.js
+++ js/join.js
@@ -3,14 +3,30 @@ $(document).ready(function () {
 	});
 });
 
-$('.multiple-items').slick({
-  infinite: true,
+
+
+$('.slider').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
 	autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed:2000,
+  dots:true,
+	arrows: true,
+	infinite: true,
 });
 
 $(".js-menu").on("click", function () {
 	$(this).next().slideToggle();
  });
+
+
+ $(window).scroll(function (){
+	$('.fadein').each(function(){
+			var elemPos = $(this).offset().top,
+					scroll = $(window).scrollTop(),
+					windowHeight = $(window).height();
+				if (scroll > elemPos - windowHeight + 100){
+						$(this).addClass('scrollin');
+					}
+			});
+	});
