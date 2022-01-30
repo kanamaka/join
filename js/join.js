f3831e2cc1a21097f474d03f91bc1087f19c4ee6
kanamaka <kubira12@gmail.com>
2022-01-31 00:58:49 +0900

    レスポンシブ

--- js/join.js
+++ js/join.js
@@ -13,12 +13,31 @@ $('.slider').slick({
   dots:true,
 	arrows: true,
 	infinite: true,
+	responsive: [{
+		breakpoint: 900,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		}
+	}]
 });
 
-$(".js-menu").on("click", function () {
+
+$('.js-menu').on("click", function () {
 	$(this).next().slideToggle();
  });
 
+
+ $(window).scroll(function (){
+	$('.fadein-top').each(function(){
+			var elemPos = $(this).offset().top,
+					scroll = $(window).scrollTop(),
+					windowHeight = $(window).height();
+				if (scroll > elemPos - windowHeight + 100){
+						$(this).addClass('scrollin');
+					}
+			});
+	});
 
  $(window).scroll(function (){
 	$('.fadein').each(function(){
