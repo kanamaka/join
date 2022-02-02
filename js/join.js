de57df02b637c5f0f562546311fb49a7e9a55d1d
kanamaka <kubira12@gmail.com>
2022-02-03 04:20:19 +0900

    end

--- js/join.js
+++ js/join.js
@@ -1,34 +1,68 @@
-$('.slider').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-	autoplay: true,
-  autoplaySpeed:2000,
-  dots:true,
-	arrows: true,
-	infinite: true,
-	responsive: [{
-		breakpoint: 768,
-		settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-		}
-	}]
+$(document).ready(function () {
+  var time = 2;
+  var $bar, $slick, isPause, tick, percentTime;
+
+  $slick = $(".slider");
+  $(".slider").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplaySpeed: 6000,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $bar = $(".slider-progress .progress");
+  function startProgressbar() {
+    resetProgressbar();
+    percentTime = 0;
+    isPause = false;
+    tick = setInterval(interval, 10);
+  }
+
+  function interval() {
+    if (isPause === false) {
+      percentTime += 1 / (time + 0.1);
+      $bar.css({
+        width: percentTime + "%",
+      });
+      if (percentTime >= 100) {
+        $slick.slick("slickNext");
+        startProgressbar();
+      }
+    }
+  }
+
+  function resetProgressbar() {
+    $bar.css({
+      width: 0 + "%",
+    });
+    clearTimeout(tick);
+  }
+
+  startProgressbar();
 });
 
+$(".js-menu").on("click", function () {
+  $(this).next().slideToggle();
+});
 
-
-
-$('.js-menu').on("click", function () {
-	$(this).next().slideToggle();
- });
-
- $(window).scroll(function (){
-	$('.fadein').each(function(){
-			var elemPos = $(this).offset().top,
-					scroll = $(window).scrollTop(),
-					windowHeight = $(window).height();
-				if (scroll > elemPos - windowHeight + 50){
-						$(this).addClass('scrollin');
-					}
-			});
-	});
+$(window).scroll(function () {
+  $(".fadein").each(function () {
+    var elemPos = $(this).offset().top,
+      scroll = $(window).scrollTop(),
+      windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight + 50) {
+      $(this).addClass("scrollin");
+    }
+  });
+});
